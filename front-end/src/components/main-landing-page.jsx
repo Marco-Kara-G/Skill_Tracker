bf6732845dc5ed939d9bdba59e1f95fdf8b2e510
Marco Grassi <marco.grassi.private@gmail.com>
2025-04-30 16:21:09 +0200

    fix: clock welcome message based on new Date

--- front-end/src/components/main-landing-page.jsx
+++ front-end/src/components/main-landing-page.jsx
@@ -2,20 +2,14 @@ import { useEffect, useState } from "react";
 import "./main-landing-page.css";
 
 export function MainPage() {
-  // set the time to loc time string
-  const [date, setDate] = useState(null);
-
   // switch between different day time
   const [dayTime, setDayTime] = useState(null);
 
   useEffect(() => {
-    // set the date to local time string
-    setDate(new Date().toLocaleTimeString());
-
     // check if the time is before 12, between 12 and 18, or after 18
     if (new Date().getHours() < 12) {
       setDayTime("Good Morning");
-    } else if (new Date().getHours() < 18 && date.getHours() >= 12) {
+    } else if (new Date().getHours() < 18 && new Date().getHours() >= 12) {
       setDayTime("Good Afternoon");
     } else {
       setDayTime("Good Evening");
@@ -43,9 +37,9 @@ export function MainPage() {
                 <path
                   d="M12 24H36M36 24L28 16M36 24L28 32"
                   stroke="#0D2F6E"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 />
               </svg>
             </button>
